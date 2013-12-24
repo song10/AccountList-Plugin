43212d93b953a212bebd8eac65f26dc0a8b7b524
song10 <song10@gmail.com>
2013-12-24 12:33:43 +0800

    + www/accountlist.js

--- www/accountlist.js
+++ www/accountlist.js
@@ -1,32 +1,39 @@
-(function() {
+cordova.define("cordova/plugin/accountlist",
+    function(require, exports, module) {
+        var exec = require("cordova/exec");
 
-    var AccountList = function() {};
+        var AccountList = function() {};
 
-    AccountList.prototype.get = function(params, success, fail) {
+        AccountList.prototype.get = function(params, success, fail) {
 
-        return Cordova.exec(
-            function(args) {
-                success(args);
-            },
-            function(args) {
-                fail(args);
-            },
-            'AccountList', '', [params]);
-    };
+            return Cordova.exec(
+                function(args) {
+                    success(args);
+                },
+                function(args) {
+                    fail(args);
+                },
+                'AccountList', '', [params]);
+        };
 
-    /*
-     * register plugin with Phonegap \ Cordova
-     */
-    if (cordova.addPlugin) {
-        cordova.addConstructor(function() {
-            //Register the javascript plugin with Cordova
-            cordova.addPlugin('AccountList', new AccountList());
-        });
-    } else {
-        if (!window.plugins) {
-            window.plugins = {};
-        }
-        window.plugins.AccountList = new AccountList();
+        var accountList = new AccountList();
+        module.exports = accountList;
+    });
+
+/*
+ * register plugin with Phonegap \ Cordova
+ */
+if (cordova.addPlugin) {
+    cordova.addConstructor(function() {
+        //Register the javascript plugin with Cordova
+        cordova.addPlugin('AccountList', new AccountList());
+    });
+} else {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    if (!window.plugins.AccountList) {
+        window.plugins.AccountList = cordova.require("cordova/plugin/accountlist");
     }
 
-})();
+}
